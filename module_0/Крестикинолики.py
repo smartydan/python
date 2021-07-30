e42f9207d08a8e0c4c9a0d0758f65ba71a456d65
smartydan <77348708+smartydan@users.noreply.github.com>
2021-07-30 15:19:56 +0300

    Update Крестикинолики.py

--- module_0/Крестикинолики.py
+++ module_0/Крестикинолики.py
@@ -1,11 +1,22 @@
 board = [["-" for i in range(3)] for j in range(3)]
 
+def greet():
+    print("Добро пожаловать в игру крестики-нолики на двоих!")
+    
+
 def show_board(board):
+    """
+    Выводит поле с текущим расположением крестиков и ноликов.
+    """ 
     for i in range(3):
         print(i + 1, *board[i])
     print(" ", 1, 2, 3)
 
+
 def take_input(player_token):
+    """
+    Принимает от пользователя координаты и размещает там соответсвующую фигуру. 
+    """
     valid = False
     while not valid:
         player_answer = input("Куда поставим " + player_token + "? ")
@@ -23,7 +34,11 @@ def take_input(player_token):
         else:
             print("Некорректный ввод. Введите координаты от 1 1 до 3 3.")
 
+
 def check(board):
+   """
+   Проверяет, нет ли выигрышной коомбинации на поле.
+   """
    win_coord = (((1, 1), (2, 1), (3, 1)), ((1, 2),(2, 2), (3, 2)), ((1, 3),(2, 3), (3, 3)), ((1, 1), (1, 2), (1, 3)),\
                 ((2, 1), (2, 2), (2, 3)), ((3, 1), (3, 2), (3, 3)), ((1, 1), (2, 2), (3, 3)), ((1, 3), (2, 2), (3, 1)))
    for i in win_coord:
@@ -31,7 +46,12 @@ def check(board):
           return board[i[0][0] - 1][i[0][1] - 1]
    return False
 
+
 def main(board):
+    """
+    Основная фунция игры.
+    """
+    greet()
     counter = 0
     win = False
     while not win:
@@ -51,6 +71,8 @@ def main(board):
             print("Ничья!")
             break
     show_board(board)
+    
+    
 main(board)
 
 input("Нажмите Enter для выхода!")
